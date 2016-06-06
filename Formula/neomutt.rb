15b7b1aa6821c7d378a8a84764b2b668a5570ccf
Richard Russon <rich@flatcap.org>
2016-06-07 00:32:56 +0100

    possible bug-fix for https://github.com/neomutt/neomutt/issues/42

--- Formula/neomutt.rb
+++ Formula/neomutt.rb
@@ -1,8 +1,8 @@
 class Neomutt < Formula
   desc "Bringing together all the Mutt Code"
   homepage "http://www.neomutt.org/"
-  url "https://github.com/neomutt/neomutt.git", :revision => "7742cc7e48b8fa967c75ecc9f5fc6609a4af1bc5"
-  version "20160530"
+  url "https://github.com/neomutt/neomutt.git", :revision => "903de2c38214837b2f4253ea5380d5a6c9bd3a88"
+  version "20160530-issue42"
   head "https://github.com/neomutt/neomutt.git", :branch => "neomutt"
 
   conflicts_with "tin",
